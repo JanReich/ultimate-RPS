321e7c023fb8761148c868c3fcff100101505772
jan.reich@mail.de <SIMS32000reich>
2018-09-26 19:54:24 +0200

    Bugfixing

--- src/gamePackage/client/menu/MultiPlayerMenu.java
+++ src/gamePackage/client/menu/MultiPlayerMenu.java
@@ -6,33 +6,33 @@ import toolBox.ImageHelper;
 
 import java.awt.event.MouseEvent;
 
-public class MultiPlayerMenu extends Menu {
+    public class MultiPlayerMenu extends Menu {
 
-            //Attribute
-        private boolean crtlPressed;
+                //Attribute
+            private boolean crtlPressed;
 
-            //Referenzen
+                //Referenzen
 
-    public MultiPlayerMenu(Display display) {
+        public MultiPlayerMenu(Display display) {
 
-        super(display);
+            super(display);
+        }
+
+        @Override
+        public void init() {
+
+            background = ImageHelper.getImage("res/images/menu/multi-menu.png");
+        }
+
+        @Override
+        public void draw(DrawHelper draw) {
+
+                //draw background
+            draw.drawImage(background, 0, 0, display.getWidth(), display.getHeight());
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent event) {
+
+        }
     }
-
-    @Override
-    public void init() {
-
-        background = ImageHelper.getImage("res/images/menu/multi-menu.png");
-    }
-
-    @Override
-    public void draw(DrawHelper draw) {
-
-            //draw background
-        draw.drawImage(background, 0, 0, display.getWidth(), display.getHeight());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent event) {
-
-    }
-}
