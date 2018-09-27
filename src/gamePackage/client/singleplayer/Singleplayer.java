a090f35432fc8c15afb33099f6fa2e67250cf041
Unterwaeschemodel123 <popelkeks46@gmail.com>
2018-09-27 18:42:43 +0200

    scoreboard

--- src/gamePackage/client/singleplayer/Singleplayer.java
+++ src/gamePackage/client/singleplayer/Singleplayer.java
@@ -1,5 +1,6 @@
 package gamePackage.client.singleplayer;
 
+import com.sun.media.jfxmedia.events.PlayerStateEvent;
 import graphics.Display;
 import graphics.interfaces.BasicInteractableObject;
 import toolBox.DrawHelper;
@@ -23,6 +24,8 @@ public class Singleplayer implements BasicInteractableObject {
     private int auswahl = 0;
     private boolean won = false;
     private boolean canPlay = true;
+    private int KIscore = 0;
+    private int playerscore = 0;
 
 
 
@@ -54,7 +57,7 @@ public class Singleplayer implements BasicInteractableObject {
             if (event.getX() >= 416 && event.getX() <= 546 && event.getY() >= 516 && event.getY() <= 646) {
                 System.out.println("Stein");
                 auswahl = auswahlStein;
-                canPlay = false;
+
                 KImove();
             }
 
@@ -62,7 +65,7 @@ public class Singleplayer implements BasicInteractableObject {
             if (event.getX() >= 546 && event.getX() <= 676 && event.getY() >= 516 && event.getY() <= 646) {
                 System.out.println("Papier");
                 auswahl = auswahlPapier;
-                canPlay = false;
+
                 KImove();
 
             }
@@ -72,7 +75,7 @@ public class Singleplayer implements BasicInteractableObject {
             if (event.getX() >= 286 && event.getX() <= 416 && event.getY() >= 516 && event.getY() <= 646) {
                 System.out.println("Schere");
                 auswahl = auswahlSchere;
-                canPlay = false;
+
                 KImove();
 
             }
@@ -84,10 +87,15 @@ public class Singleplayer implements BasicInteractableObject {
         int KIauswahl = (int)(Math.random()*3+1);
         if(auswahl==1&&KIauswahl==3||auswahl==2&&KIauswahl==1||auswahl==3&&KIauswahl==2){
             won = true;
+            playerscore = playerscore +1;
+            System.out.println("Gewonnen");
         }else if(auswahl==KIauswahl){
             canPlay = true;
+            System.out.println("Unentschieden");
         }else{
             won = false;
+            KIscore = KIscore + 1;
+            System.err.println("Verloren");
         }
 
     }
@@ -104,5 +112,6 @@ public class Singleplayer implements BasicInteractableObject {
             draw.drawImage(schere, 286, 516, 128, 128);
             draw.drawImage(papier, 546, 516, 128, 128);
         }
+        draw.drawString("Player: "+ playerscore +" ----- KI: "+KIscore,370,50);
     }
 }
